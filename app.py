2359068e1177bfa80083a6283d3cf9d38a98f08f
David Chao <dchao104@gmail.com>
2019-12-23 16:59:21 -0600

    upload html codes and app.py

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/education_employed")
+@app.route("/pyspark")
 def education_employed():
-    return render_template("education_employed.html")
+    return render_template("pyspark.html")
 
 
-@app.route("/edu_employed_d3")
+@app.route("/pyspark_iframe")
 def edu_employed_d3():
     return render_template("edu_employed_d3.html")
 
