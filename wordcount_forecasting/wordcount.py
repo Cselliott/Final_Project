10dcb679f1a1f50d66ea04588e9064fb3c57740c
David Chao <dchao104@gmail.com>
2019-12-28 16:51:15 -0600

    upload word_count and web_scrapping

--- wordcount_forecasting/wordcount.py
+++ wordcount_forecasting/wordcount.py
@@ -1,18 +1,6 @@
 #
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Change to the Final_Project\wordcount_forecasting folder
+# Execute the command:  spark-submit wordcount.py
 #
 
 from __future__ import print_function
@@ -25,7 +13,7 @@ import csv
 
 sys.stdout.reconfigure(encoding='utf-8')
 
-# for root, dirs, files in os.walk("cleaned/"):
+# for root, dirs, files in os.walk("../cleaned/"):
 #     for input_file in files:
 #         file = input_file.split("meeting")
 #         timestamp = file[0].split("FOMC")
@@ -46,24 +34,24 @@ if __name__ == "__main__":
         .appName("PythonWordCount")\
         .getOrCreate()
 
-    myFile = open('results/pyspark.csv', 'w')
+    myFile = open('wordcount.csv', 'w')
     with myFile:
         myFields = ["timestamp", "keyword", "count"]
         writer = csv.DictWriter(myFile, fieldnames=myFields)
         writer.writeheader()
 
-        for root, dirs, files in os.walk("cleaned/"):
+        for root, dirs, files in os.walk("../cleaned/"):
             for input_file in files:
                 file = input_file.split("meeting")
                 timestamp = file[0].split("FOMC")
-                # print(timestamp[1])
+                print(timestamp[1])
 
                 # exit()
 
                 # print("cleaned/" + input_file)
 
                 lines = spark.read.option('encoding', 'utf-8').text(
-                    "cleaned/" + input_file).rdd.map(lambda r: r[0])
+                    "../cleaned/" + input_file).rdd.map(lambda r: r[0])
 
                 counts = lines.flatMap(lambda x: x.split(' ')) \
                     .map(lambda x: (x, 1)) \
@@ -71,10 +59,11 @@ if __name__ == "__main__":
 
                 output = counts.collect()
 
-                keyWords = ["recession", "unemployment", "inflation"]
+                keyWords = ["recession", "unemployment",
+                            "inflation", "interest"]
 
-                # print("\n")
-                # print("**********************")
+                print("\n")
+                print("**********************")
 
                 for (word, count) in sorted(output):
                     if word in keyWords:
@@ -82,7 +71,7 @@ if __name__ == "__main__":
                         writer.writerow(
                             {'timestamp': timestamp[1], 'keyword': word, 'count': count})
 
-                # print("**********************\n")
+                print("**********************\n")
 
                 # exit()
 
